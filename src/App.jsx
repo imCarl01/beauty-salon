9406c385578559610530346c22004ab6ab8a5cca
imCarl01 <imcarl0.uc@gmail.com>
2025-10-21 09:54:47 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 const App = () => {
   return (
+    
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
